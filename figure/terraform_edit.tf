4feb5531b8a2e2469521e19aa4715f4a989b2cef
Ty2 <92143695+Tianyi2@users.noreply.github.com>
2026-03-30 08:06:18 +1100

    Update figure content.

--- figure/terraform_edit.tf
+++ figure/terraform_edit.tf
@@ -1,12 +1,14 @@
-resource "aws_security_group" "db" {
+resource "aws_security_group" "db_security_group" {
   ingress {
-    cidr_blocks = ["0.0.0.0/0"]             # SMELL 1
+    cidr_blocks = ["0.0.0.0/0"]             # SMELL 1 - Unrestricted IP addres
   }
 }
 resource "aws_db_instance" "database" {
   count = var.environment == "prod" ? 1 : 0
+  vpc_security_group_ids = [
+    aws_security_group.db_security_group.id]
 }
-resource "aws_cloudwatch_metric_alarm" "db_cpu" { # SMELL 2
+resource "aws_cloudwatch_metric_alarm" "db_cpu" { # SMELL 2 - Cascading Provision Failure
   dimensions = {
     DBInstanceIdentifier = aws_db_instance.database[0].id
   }
